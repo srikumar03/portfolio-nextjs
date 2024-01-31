c1ff5cf1940cae26fef1a34c2c3ea3164660a54d
SRIDHAR K <srikumarkgm369@gmail.com>
2024-02-01 01:30:39 +0530

    s

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Layout = ({ children, className = "" }) => {
-  return <div className={`${className}`}>{children}</div>;
+  return (
+    <div
+      className={`w-full h-full inline-block z-0 bg-light p-32 ${className}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Layout from "@/components/Layout";
+import sri_avatar from "../../public/images/profile/Sri_rbg_final1.png";
+import sri_avatar2 from "../../public/images/profile/developer-pic-1.png";
+import avatar from "../../public/images/profile/avatar_Rbg.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,22 @@ export default function Home() {
         <meta name="description" content="Sridhar Kumar's Portfolio" />
       </Head>
       <main className="flex items-center text-dark w-full min-h-screen">
-        <Layout>Hi da monnai</Layout>
+        <Layout>
+          <div className="flex items-center justify-between w-full">
+            <div className="w-1/2">
+              <Image src={sri_avatar} alt="sri" className="w-full h-auto" />
+            </div>
+            <div>
+              <h1>Turning Vision Into Reality With Code And Design</h1>
+              <p>
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development
+              </p>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   );
