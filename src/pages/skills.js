39b719aab2f72ee7c10fde7ae2dac0dcdfe901fe
SRIDHAR K <srikumarkgm369@gmail.com>
2024-12-02 22:35:14 +0530

    v1.1.2

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -6,10 +6,25 @@ import { motion } from "framer-motion";
 import Transition from "@/components/Transition";
 
 const skillsData = {
-  "Frontend Languages": ["Reactjs", "Nextjs", "Angularjs", "Bootstrap", "Tailwind"],
+  "Frontend Languages": [
+    "Reactjs",
+    "Nextjs",
+    "Angularjs",
+    "Bootstrap",
+    "Tailwind",
+  ],
   "Backend Languages": ["MySQL", "PHP", "Node.js", "MongoDB"],
   "Programming Languages": ["Java", "Python"],
-  "Tool Stack": ["Figma", "Adobe Photoshop", "Git", "Postman", "UIPath", "Selenium", "Netlify / Vercel"]
+  "Web Solutions / Content Management": ["Wordpress", "Woocommerce", "Shopify"],
+  "Tool Stack": [
+    "Figma",
+    "Adobe Photoshop",
+    "Git",
+    "Postman",
+    "UIPath",
+    "Selenium",
+    "Netlify / Vercel",
+  ],
 };
 
 const Project = ({ type }) => (
@@ -17,7 +32,12 @@ const Project = ({ type }) => (
     whileTap={{ scale: 0.8 }}
     animate={{ scale: 1 }}
     initial={{ scale: 5 }}
-    transition={{ ease: "easeOut", type: "spring", stiffness: 100 }}
+    transition={{
+      ease: "easeOut",
+      type: "spring",
+      stiffness: 100,
+      delay: 0.5,
+    }}
     className="m-3 md:m-2"
   >
     <article className="relative w-full flex flex-col items-center justify-between rounded-md border border-solid border-dark bg-light p-1 dark:bg-dark dark:border-light xs:p-4">
@@ -37,7 +57,9 @@ const SkillsSection = ({ title, skills }) => (
       {title}
     </h2>
     <div className="flex items-center justify-center flex-wrap">
-      {skills.map(skill => <Project key={skill} type={skill} />)}
+      {skills.map((skill) => (
+        <Project key={skill} type={skill} />
+      ))}
     </div>
   </div>
 );
@@ -46,12 +68,18 @@ const Skills = () => (
   <>
     <Head>
       <title>Sridhar | Skills</title>
-      <meta name="description" content="Sridhar's skills page showcasing expertise in various frontend, backend, programming languages and tools." />
+      <meta
+        name="description"
+        content="Sridhar's skills page showcasing expertise in various frontend, backend, programming languages and tools."
+      />
     </Head>
     <Transition />
     <main className="w-full mb-1 flex flex-col items-center justify-center dark:text-light">
       <Layout className="pt-16">
-        <AnimatedText className="mb-16 z-10 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" text="Skills Unleashed, Expertise Evolved!" />
+        <AnimatedText
+          className="mb-16 z-10 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          text="Skills Unleashed, Expertise Evolved!"
+        />
         {Object.entries(skillsData).map(([title, skills]) => (
           <SkillsSection key={title} title={title} skills={skills} />
         ))}
